33176902e43baf340fd905caee0f45a6db3fbfc7
Upendra Dhakal <dhakal.upenn@gmail.com>
2026-06-20 13:34:43 -0400

    add running total example

---
+++ sales/queries/commulative_sales.sql
@@ -0,0 +1,7 @@
+WITH daily_sales as (
+    SELECT sold_on, SUM(price) total
+    FROM sales
+    GROUP BY sold_on
+)
+select sold_on, total, SUM(total) OVER (ORDER BY sold_on ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as running_total
+from daily_sales
